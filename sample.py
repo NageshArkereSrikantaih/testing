207dbf9ff42a21f080b322c8d892b3f6ef6eb312
Nagesh <nagesh.arkere.srikantaiah@oracle.com>
2023-11-05 23:39:37 +0530

    basic_code

---
+++ sample.py
@@ -0,0 +1,17 @@
+message="hello World!"
+print(message)
+
+#strip
+first_name="nagesh"
+last_name="arkere"
+full_name=f"{first_name}{last_name}"
+
+#whitespace and next line
+
+full_name_add_whitespace=full_name
+print("\t\nfull_name_add_whitespace")
+
+#strip
+
+filtering='\tfull_name'
+print(filtering.lstrip())
